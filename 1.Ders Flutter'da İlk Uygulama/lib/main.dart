14466056cc9baa645f7b67341b5243a21f1d50b6
Emre <47086805+karalogluemre@users.noreply.github.com>
2022-01-05 23:43:26 +0300

    Add files via upload

---
+++ 1.Ders Flutter'da İlk Uygulama/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void Main() {
+  //fonksiyon
+  runApp(MaterialApp(home: MyApp()
+      //
+      ));
+}
+
+class MyApp extends StatelessWidget {
+  String mesaj = "Benim ilk Uygulamam!";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(mesaj),
+      ),
+      body: Center(
+        child: RaisedButton(onPressed: () {
+          var alert = AlertDialog(
+            title: Text("Sınav Sonucu"),
+            content: Text("Geçti"),
+          );
+          showDialog(
+              context: context, builder: (BuildContext context) => alert
+              );
+        }),
+      ),
+    );
+  }
+}
