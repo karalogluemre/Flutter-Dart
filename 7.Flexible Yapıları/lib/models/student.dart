218a7efe3442a38641959339c7810a40a3b9f0ea
Emre <47086805+karalogluemre@users.noreply.github.com>
2022-01-07 17:29:52 +0300

    Add files via upload

---
+++ 7.Flexible Yapıları/lib/models/student.dart
@@ -0,0 +1,15 @@
+class Student{
+  String firstName;
+  String lastName;
+  int yas;
+  String status;
+  int grade;
+
+  Student(String firstName,String lastName,int yas,int grade){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.yas = yas;
+    this.status = "Gecti";
+    this.grade = grade;
+  }
+}
